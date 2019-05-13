266d8e2a3967daadfa8cc6057dccd90ab3e1291e
spezi77 <spezi7713@gmx.net>
2019-05-14 00:45:53 +0200

    LS Clock Font size: Update defaults [2/2].
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/aosip/owlsnest/lockscreen/OptionsCategory.java
+++ src/com/aosip/owlsnest/lockscreen/OptionsCategory.java
@@ -79,13 +79,13 @@ public class OptionsCategory extends SettingsPreferenceFragment implements
         // Lock Clock Size
         mClockFontSize = (CustomSeekBarPreference) findPreference(CLOCK_FONT_SIZE);
         mClockFontSize.setValue(Settings.System.getInt(resolver,
-                Settings.System.LOCKCLOCK_FONT_SIZE, 78));
+                Settings.System.LOCKCLOCK_FONT_SIZE, 68));
         mClockFontSize.setOnPreferenceChangeListener(this);
 
         // Lock Date Size
         mDateFontSize = (CustomSeekBarPreference) findPreference(DATE_FONT_SIZE);
         mDateFontSize.setValue(Settings.System.getInt(resolver,
-                Settings.System.LOCKDATE_FONT_SIZE,14));
+                Settings.System.LOCKDATE_FONT_SIZE,16));
         mDateFontSize.setOnPreferenceChangeListener(this);
     }
 
